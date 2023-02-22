d2677013de5b66c13c6359057f938b1c6fd81776
tanukile <backpacker320@gmail.com>
2023-02-22 12:23:24 +0800

    取得產品列表

--- cart.js
+++ cart.js
@@ -1,7 +1,40 @@
-import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.esm-browser.min.js';
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.esm-browser.min.js";
+
+const apiUrl = "https://vue3-course-api.hexschool.io/v2";
+const apiPath = "vuejslive2022";
 
 const app = createApp({
-
+  data() {
+    return {
+			products: [],
+		};
+  },
+	methods: {
+		getProducts() {
+			axios.get(`${apiUrl}/api/${apiPath}/products`)
+				.then(res => {
+					console.log('產品列表:',res.data.products);
+					this.products = res.data.products;
+				})
+				.catch(err => {
+					alert(err.data.message);
+				})
+		}
+	},
+	mounted() {
+		this.getProducts();
+	}
 });
+// 產品 modal 元件
+const productModal = {
+	data() {
+		return {
+			// 暫存產品資料
+			tempProduct: {},
+		};
+	},
+	template: '#userProductModal',
+};
 
-app.mount('#app');
+app.component('productModal', {productModal});
+app.mount("#app");
